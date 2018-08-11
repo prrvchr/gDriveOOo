6abd553a5e9c137c7d591fbf6f41608259152bfb
User <prrvchr@gmail.com>
2018-08-11 15:34:44 +0200

    identifiers

---
+++ gDriveOOo/pythonpath/gdrive/identifiers.py
@@ -0,0 +1,46 @@
+#!
+# -*- coding: utf_8 -*-
+
+from .google import IdGenerator
+
+
+def getIdDelete(connection):
+    query = 'DELETE FROM "Identifiers" WHERE "UserName" = ? AND "Id" = ?;'
+    return connection.prepareStatement(query)
+
+def getIdSelect(connection):
+    query = 'SELECT "Id", "TimeStamp" FROM "Identifiers" WHERE "InUse" = FALSE AND "UserName"= ? ORDER BY "TimeStamp" LIMIT 1;'
+    return connection.prepareStatement(query)
+
+def getIdInsert(connection):
+    query = 'INSERT INTO "Identifiers" ("UserName", "Id") VALUES (?, ?);'
+    return connection.prepareStatement(query)
+
+def getIdUpdate(connection):
+    query = 'UPDATE "Identifiers" SET "InUse" = TRUE, "TimeStamp" = CURRENT_TIMESTAMP(3) WHERE "Id" = ?;'
+    return connection.prepareStatement(query)
+
+def updateIdentifier(update, id):
+    update.setString(1, id)
+    return update.executeUpdate()
+    
+def getNewId(ctx, scheme, username, select, insert):
+    select.setString(1, username)
+    result = select.executeQuery()
+    if result.next():
+        id = result.getString(1)
+    else:
+        id = _insertNewId(ctx, scheme, username, insert, select)
+    result.close()
+    return id
+
+def _insertNewId(ctx, scheme, username, insert, select):
+    insert.setString(1, username)
+    for id in IdGenerator(ctx, scheme, username):
+        insert.setString(2, id)
+        insert.executeUpdate()
+    result = select.executeQuery()
+    if result.next():
+        id = result.getString(1)
+    result.close()
+    return id
