5e5aabb791153fcf8f134e1d617b08144c6d2e02
User <prrvchr@gmail.com>
2019-09-24 20:32:20 +0200

    new FileSyncModes and FolderSyncModes

--- gDriveOOo/Provider.py
+++ gDriveOOo/Provider.py
@@ -11,6 +11,13 @@ from com.sun.star.auth.RestRequestTokenType import TOKEN_URL
 from com.sun.star.auth.RestRequestTokenType import TOKEN_REDIRECT
 from com.sun.star.auth.RestRequestTokenType import TOKEN_QUERY
 from com.sun.star.auth.RestRequestTokenType import TOKEN_JSON
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_RETRIEVED
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_CREATED
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_FOLDER
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_FILE
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_RENAMED
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_REWRITED
+from com.sun.star.ucb.RestDataSourceSyncMode import SYNC_TRASHED
 
 # clouducp is only available after CloudUcpOOo as been loaded...
 try:
@@ -81,9 +88,6 @@ class Provider(ProviderBase):
     @property
     def TimeStampPattern(self):
         return '%Y-%m-%dT%H:%M:%S.00'
-    @property
-    def GenerateIds(self):
-        return True
     @property
     def IdentifierRange(self):
         return g_IdentifierRange
