1ede4eb58c160cec0f14124e8ae08f2deaf7a94c
User <prrvchr@gmail.com>
2019-12-09 18:45:58 +0100

    Symbolic links in Git

--- CloudUcpOOo/CloudUcpOOo/pythonpath/clouducp/__init__.py
+++ CloudUcpOOo/CloudUcpOOo/pythonpath/clouducp/__init__.py
@@ -3,27 +3,6 @@
 
 import traceback
 try:
-    from .configuration import g_oauth2
-    from .configuration import g_scheme
-    from .configuration import g_plugin
-    from .configuration import g_provider
-    from .configuration import g_host
-    from .configuration import g_url
-    from .configuration import g_upload
-    from .configuration import g_folder
-    from .configuration import g_link
-    from .configuration import g_doc_map
-    from .configuration import g_office
-    from .configuration import g_pages
-    from .configuration import g_userkeys
-    from .configuration import g_userfields
-    from .configuration import g_itemkeys
-    from .configuration import g_itemfields
-    from .configuration import g_capabilitykeys
-    from .configuration import g_childfields
-    from .configuration import g_IdentifierRange
-    from .configuration import g_chunk
-    from .configuration import g_buffer
 
     from .datasource import DataSource
     from .user import User
